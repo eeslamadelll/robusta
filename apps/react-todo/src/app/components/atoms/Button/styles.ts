a5337086862a82bcc1e5e7763162460b9db3244d
Eslam Adel <eslamadel@MacBook-Pro-3.local>
2022-09-17 22:22:09 +0200

    fix: button position

--- apps/react-todo/src/app/components/atoms/Button/styles.ts
+++ apps/react-todo/src/app/components/atoms/Button/styles.ts
@@ -2,11 +2,9 @@ import styled from 'styled-components';
 
 export const Button = styled.button`
   background-color: var(--color-primary);
-  position: fixed;
-  bottom: 60px;
-  right: calc(100% - 84%);
-  // bottom: 20px;
-  // right: 20px;
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
   width: 65px;
   height: 65px;
   border: none;
